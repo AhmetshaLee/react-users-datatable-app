7259e37dfeac38bf09a0dea0a63446551733bf54
Sharipov Akhmetsha <scharipov.arsen2013@yandex.ru>
2024-07-23 00:08:11 +0500

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: command === 'build' ? '/REPO-NAME/' : '/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   css: {
     modules: {
       localsConvention: 'camelCaseOnly',
